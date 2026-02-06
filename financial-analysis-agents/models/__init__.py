8a86b880092626f4ab8a95e65b839807bdf7e81f
wolf_av_1948_1992 <158048847+giuseppedavidde@users.noreply.github.com>
2026-02-06 21:07:47 +0100

    Delete financial-analysis-agents directory

--- financial-analysis-agents/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# models/__init__.py
-
-from .data_schema import FinancialData
-
-# Definisce cosa viene esportato quando qualcuno fa "from models import *"
-__all__ = ["FinancialData"]
--- financial-analysis-agents/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# utils/__init__.py
-
-from .loader import load_company_data
-from .cache_manager import CacheManager
-
-__all__ = ["load_company_data","CacheManager"]
